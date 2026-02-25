99b82abda47ecb5299e32859d8fb77c3cf673f6f
Lucy-Bernard <lbernar5@depaul.edu>
2026-02-25 11:25:34 -0600

    finished logic to render gift data to UI

--- client/public/scripts/gifts.js
+++ client/public/scripts/gifts.js
@@ -1,19 +1,43 @@
 const renderGifts = async () => {
   const response = await fetch("/gifts");
   const data = await response.json();
-  console.log(data);
+  console.log("all gifts", data);
   if (data) {
     data.map((gift) => {
+      console.log("gift", gift);
       const card = document.createElement("div");
       card.classList.add("card");
 
       const topContainer = document.createElement("div");
       topContainer.classList.add("top-container");
+      topContainer.style.backgroundImage = `url(${gift.image})`;
 
       const bottomContainer = document.createElement("div");
       bottomContainer.classList.add("bottom-container");
+
+      const giftName = document.createElement("h3");
+      giftName.textContent = gift.name;
+      bottomContainer.appendChild(giftName);
+
+      const giftAudience = document.createElement("p");
+      giftAudience.textContent = `Greate For: ${gift.audience}`;
+      bottomContainer.appendChild(giftAudience);
+
+      const readMoreLink = document.createElement("a");
+      readMoreLink.textContent = "Read More > ";
+      readMoreLink.href = `/gifts/${gift.id}`;
+      bottomContainer.appendChild(readMoreLink);
+
+      card.appendChild(topContainer);
+      card.appendChild(bottomContainer);
+      mainContent.appendChild(card);
     });
+  } else {
+    const errorMessage = document.createElement("h2");
+    errorMessage.textContent = "No Gifts Available :( ";
+    mainContent.appendChild(errorMessage);
   }
 };
 
 const mainContent = document.getElementById("main-content");
+renderGifts();
